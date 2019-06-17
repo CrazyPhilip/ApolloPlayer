1434cae174ccb46690d3597346d5cb97becc3efe
飞 程 <cf20090901@outlook.com>
2019-06-17 21:26:46 +0800

    1.新增“修改默认目录”功能

--- ApolloPlayer/SettingsWindow.xaml.cs
+++ ApolloPlayer/SettingsWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using SourceChord.FluentWPF;
+using System.IO;
+using System.Windows.Forms;
 
 namespace ApolloPlayer
 {
@@ -10,6 +12,20 @@ namespace ApolloPlayer
         public SettingsWindow()
         {
             InitializeComponent();
+        }
+
+        private void SelectPathButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            FolderBrowserDialog path = new FolderBrowserDialog();
+            path.ShowDialog();
+            PathTextBlock.Text = path.SelectedPath;
+
+            string log_path = "./default_path.dat";
+            FileStream fs = new FileStream(log_path, FileMode.Create, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.Write(path.SelectedPath);
+            sw.Close();
+            fs.Close();
         }
     }
 }
--- ApolloPlayer/ViewModel/MusicListViewModel.cs
+++ ApolloPlayer/ViewModel/MusicListViewModel.cs
@@ -8,6 +8,7 @@ using ApolloPlayer.Model;
 using Shell32;
 using Microsoft.Win32;
 using Microsoft.Practices.Prism.Commands;
+using System.IO;
 
 namespace ApolloPlayer.ViewModel
 {
@@ -40,19 +41,39 @@ namespace ApolloPlayer.ViewModel
 
         public MusicListViewModel()
         {
+            Init();   //初始化，读取默认目录
+
             this.AddMusicCommand = new DelegateCommand(new Action(this.AddMusicCommandExecute));
             this.ShuffleCommand = new DelegateCommand(new Action(this.ShuffleCommandExecute));
         }
         
         /// <summary>
+        /// 初始化，读取默认目录
+        /// </summary>
+        private void Init()
+        {
+            string default_path;
+            string log_path = "./default_path.dat";
+
+            if (File.Exists(log_path))
+            {
+                StreamReader sr = new StreamReader(log_path);
+                default_path = sr.ReadLine();
+            }
+            else
+            {
+                default_path = (Environment.GetFolderPath(Environment.SpecialFolder.MyMusic)).ToString();
+            }
+            
+            string[] files = Directory.GetFiles(default_path, "*.mp3");
+            AddMusic(files);
+        }
+
+        /// <summary>
         /// 选择音频文件
         /// </summary>
         private void AddMusicCommandExecute()
         {
-            ShellClass shell = new ShellClass();
-            List<MusicInfo> temp_list = new List<MusicInfo>();
-            MusicInfo temp;
-
             OpenFileDialog dialog = new OpenFileDialog();
             dialog.Multiselect = true;
             dialog.Title = "请选择文件";
@@ -61,24 +82,37 @@ namespace ApolloPlayer.ViewModel
             var result = dialog.ShowDialog();
             if (result == true)
             {
-                foreach (string file in dialog.FileNames)
-                {
-                    temp = new MusicInfo();
-                    Folder dir = shell.NameSpace(System.IO.Path.GetDirectoryName(file));
-                    FolderItem item = dir.ParseName(System.IO.Path.GetFileName(file));
-                    
-                    temp.file_name = dir.GetDetailsOf(item, 0);
-                    temp.music_title = dir.GetDetailsOf(item, 21);
-                    temp.album = dir.GetDetailsOf(item, 14);
-                    temp.artist = dir.GetDetailsOf(item, 13);
-                    temp.length = dir.GetDetailsOf(item, 27);
-                    temp.size = dir.GetDetailsOf(item, 1);
-                    temp.file_path = file;
-                    
-                    temp_list.Add(temp);
-                }
-                Music_List = temp_list;
+                AddMusic(dialog.FileNames);
             }
+        }
+
+        /// <summary>
+        /// 添加音乐文件信息
+        /// </summary>
+        /// <param name="files"></param>
+        private void AddMusic(string[] files)
+        {
+            MusicInfo temp;
+            List<MusicInfo> temp_list = new List<MusicInfo>();
+            ShellClass shell = new ShellClass();
+
+            foreach (string file in files)
+            {
+                temp = new MusicInfo();
+                Folder dir = shell.NameSpace(Path.GetDirectoryName(file));
+                FolderItem item = dir.ParseName(Path.GetFileName(file));
+
+                temp.file_name = dir.GetDetailsOf(item, 0);
+                temp.music_title = dir.GetDetailsOf(item, 21);
+                temp.album = dir.GetDetailsOf(item, 14);
+                temp.artist = dir.GetDetailsOf(item, 13);
+                temp.length = dir.GetDetailsOf(item, 27);
+                temp.size = dir.GetDetailsOf(item, 1);
+                temp.file_path = file;
+
+                temp_list.Add(temp);
+            }
+            Music_List = temp_list;
         }
         
         private void ShuffleCommandExecute()
